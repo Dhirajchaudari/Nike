78d983aea504d4ddb66f1c261744c6a48425d51e
Dhirajchaudari <dhirajchaudhari789@gmail.com>
2023-10-01 11:45:17 +0530

    added the dist

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'https://github.com/Dhirajchaudari/Nike.git',
+  base:'/Nike/',
   plugins: [react()],
 })
